dc4e2a5aa08b73f375e3d316782173aa6e1fa855
harishm <harishm762003@gmail.com>
2023-12-16 09:26:39 +0530

    Changed the CardCarousal component in search page.

---
+++ pages/Search/Components/CardCarousal.jsx
@@ -0,0 +1,73 @@
+// import { Dimensions } from "react-native";
+import { Text } from "react-native";
+import { View, Image, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import Carousel from "react-native-reanimated-carousel";
+
+export default function CardCarousel({ title, content, price }) {
+  const width = Dimensions.get("window").width;
+  return (
+    <View style={{ borderRadius: 10, flex: 1 }}>
+      <Carousel
+        loop
+        width={width}
+        height={width / 2}
+        autoPlay={false}
+        data={[...new Array(6).keys()]}
+        scrollAnimationDuration={1000}
+        // onSnapToItem={(index) => console.log("current index:", index)}
+        renderItem={({ index }) => (
+          <View style={{ paddingHorizontal: 5, flex: 1 }}>
+            {/* <Image
+              height={"98%"}
+              width={"93%"}
+              style={{borderRadius : 10 , margin : 5 }}
+              source={{
+                uri: "https://s3images.zee5.com/wp-content/uploads/2021/12/avatar-2-kate-winslet-wears-wings-underwater-for-her-sea-per_361y.jpg",
+              }}
+            /> */}
+            <View
+              style={{
+                borderWidth: 0.5,
+                borderColor: "rgba(225,225,225,0.6)",
+                borderRadius: 5,
+                flex: 1,
+                padding: 10,
+                justifyContent: "space-between",
+                gap: 10,
+              }}
+            >
+              <Text style={{ fontSize: 25, fontWeight: "bold" }}>{title}</Text>
+              <Text numberOfLines={2}>{content}</Text>
+              <View
+                style={{
+                  alignItems: "flex-end",
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems : "center",
+                  paddingHorizontal: 5,
+                }}
+              >
+                <Text>Price : {price}</Text>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: "black",
+                    padding: 10,
+                    borderRadius: 5,
+                    justifyContent: "center",
+                    alignItems: "flex-end",
+                    paddingHorizontal: 10,
+                  }}
+                >
+                  <Text style={{ color: "white" }}>View</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
--- pages/Search/Search.jsx
+++ pages/Search/Search.jsx
@@ -2,41 +2,56 @@ import { TextInput } from "react-native";
 // import { Image } from "react-native";
 // import { View, Text } from "react-native";
 
-import { View } from 'react-native';
+import { View } from "react-native";
 import SearchCarousel from "./Components/Carousal";
 import { Text } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import CardCarousel from "./Components/CardCarousal";
 
 export default function Search() {
   return (
-    <ScrollView style={{flex : 1 }}>
-      <View style={{ flex: 1 , padding : 10 , gap : 30 , backgroundColor : "white"}}>
-      <View style={{paddingVertical : "5%" , paddingHorizontal : "2%" }}>
-        <Text style={{fontSize : 30 , fontWeight : "bold"}}>Search</Text>
-      </View>
-      <TextInput
-        placeholder="Search by name.."
-        style={{ backgroundColor: "rgba(225,225,225,0.5)" , padding : "2%" , borderRadius : 10 }}
-      />
-      <View style={{flex : 0.8}}>
+    <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+      <View style={{ flex: 1, padding: 10, gap: 30, backgroundColor: "white" }}>
+        <View style={{ paddingVertical: "5%", paddingHorizontal: "2%" }}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Search</Text>
+        </View>
+        <TextInput
+          placeholder="Search by name.."
+          style={{
+            backgroundColor: "rgba(225,225,225,0.5)",
+            padding: "2%",
+            borderRadius: 10,
+          }}
+        />
+        <View style={{ flex: 0.8 }}>
+          <SearchCarousel />
+        </View>
+        <View style={{ paddingHorizontal: "2%" }}>
+          <Text style={{ fontSize: 30 }}>Registered</Text>
+        </View>
+        <View style={{ flex: 0.8 }}>
+          <CardCarousel
+            title={"walkathon"}
+            price={100}
+            content={
+              "Avatar, a cinematic masterpiece by James Cameron, immerses audiences in the breathtaking world of Pandora, where the Na'vi people and their connection to nature unfold a mesmerizing tale of love and environmental consciousness"
+            }
+          />
+        </View>
 
-      <SearchCarousel/>
-      </View>
-      <View style={{paddingHorizontal : "2%"}}>
-        <Text style={{fontSize : 30 }}>Registered</Text>
-      </View>
-      <View style={{flex : 1}} >
-        <SearchCarousel/>
-      </View>
-      
-      <View style={{paddingHorizontal : "2%"}}>
-        <Text style={{fontSize : 30 }}>New Arrival</Text>
-      </View>
-      <View style={{flex : 1}} >
-        <SearchCarousel/>
-      </View>
+        <View style={{ paddingHorizontal: "2%" }}>
+          <Text style={{ fontSize: 30 }}>New Arrival</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <CardCarousel
+          price={100}
+            title={"walkathon"}
+            content={
+              "Avatar, a cinematic masterpiece by James Cameron, immerses audiences in the breathtaking world of Pandora, where the Na'vi people and their connection to nature unfold a mesmerizing tale of love and environmental consciousness"
+            }
+          />
+        </View>
       </View>
     </ScrollView>
-    
   );
 }
